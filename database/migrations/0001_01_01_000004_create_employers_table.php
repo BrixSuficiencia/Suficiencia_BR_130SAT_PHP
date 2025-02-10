1331e2168576228d9bb2b40b3abe8fcec579ac22
unknown <brixsuficiencia@gmail.com>
2025-02-10 16:39:38 +0800

    0208_10_Model Factories

--- database/migrations/0001_01_01_000004_create_employers_table.php
+++ database/migrations/0001_01_01_000004_create_employers_table.php
@@ -1,7 +1,9 @@
 <?php
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+
 return new class extends Migration
 {
     /**
@@ -9,18 +11,18 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('job_listings', function (Blueprint $table) {
+        Schema::create('employers', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
-            $table->string('salary');
+            $table->string('name');
             $table->timestamps();
         });
     }
+    
     /**
      * Reverse the migrations.
      */
     public function down(): void
     {
-        Schema::dropIfExists('job_listings');
+        Schema::dropIfExists('employers');
     }
 };

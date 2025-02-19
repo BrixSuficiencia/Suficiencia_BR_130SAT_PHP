1437676c2fbf0d92bdf732fc763851430adc6d56
unknown <brixsuficiencia@gmail.com>
2025-02-19 17:34:06 +0800

    0215_24_Programming is Rewriting_Lec

--- controllers/note.php
+++ controllers/note.php
@@ -8,11 +8,7 @@ $currentUserId = 1;
 
 $note = $db->query('SELECT * FROM notes WHERE id = :id', [
     'id' => $_GET['id']
-])->fetch();
-
-if (! $note) {
-    abort();
-}
+])->findOrFail();
 
 if ($note['user_id'] != $currentUserId) {
     abort(Response::FORBIDDEN);
--- functions.php
+++ functions.php
@@ -1,6 +1,7 @@
 <?php
 
-function dd($value) {
+function dd($value) 
+{
     echo "<pre>";
     var_dump($value);
     echo "</pre>";
@@ -10,4 +11,10 @@ function dd($value) {
 
 function urlIs($value) {
     return $_SERVER['REQUEST_URI'] === $value;
+}
+
+function authorize($condition, $status = Response::FORBIDDEN) {
+    if (! $condition) {
+        abort($status);
+    }
 }

ea8d8cb915edf7d2b5a2787a4d66eae0d89eb2c8
unknown <brixsuficiencia@gmail.com>
2025-03-08 15:38:29 +0800

    0308_22_Make a Login and Registration System from scratch part 2_Lab

--- resources/views/Components/layout.blade.php
+++ resources/views/Components/layout.blade.php
@@ -26,9 +26,17 @@
         <div class="hidden md:block">
           <div class="ml-4 flex items-center md:ml-6">
             @guest
-               <x-nav-link href="/login" :active="request()->is('login')">Log In</x-nav-link>
-               <x-nav-link href="/register" :active="request()->is('register')">Register</x-nav-link>
+              <x-nav-link href="/login" :active="request()->is('login')">Log In</x-nav-link>
+              <x-nav-link href="/register" :active="request()->is('register')">Register</x-nav-link>
             @endguest
+
+            @auth
+              <form method="POST" action="/logout">
+                @csrf
+
+                <x-form-button>Log Out</x-form-button>
+              </form>
+            @endauth
           </div>
         </div>
         <div class="-mr-2 flex md:hidden">
@@ -52,7 +60,7 @@
     <!-- Mobile menu, show/hide based on menu state. -->
     <div class="md:hidden" id="mobile-menu">
       <div class="space-y-1 px-2 pb-3 pt-2 sm:px-3">
-      <x-nav-link href="/" :active="request()->is('/')">Home</x-nav-link>
+        <x-nav-link href="/" :active="request()->is('/')">Home</x-nav-link>
         <x-nav-link href="/jobs" :active="request()->is('jobs')">Jobs</x-nav-link>
         <x-nav-link href="/contact" :active="request()->is('contact')">Contact</x-nav-link>
       </div>
@@ -78,9 +86,9 @@
   </nav>
 
   <header class="bg-white shadow">
-  <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8 sm:flex sm:justify-between">
+    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8 sm:flex sm:justify-between">
       <h1 class="text-3xl font-bold tracking-tight text-gray-900">{{ $heading }}</h1>
-    
+
       <x-button href="/jobs/create">Create Job</x-button>
     </div>
   </header>
